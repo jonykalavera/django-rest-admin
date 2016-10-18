79b3daf6b4bed7b2200ab85e300a39adbfb64e35
Tato <tato.altamirano@gmail.com>
2016-10-18 15:36:20 -0300

    initial static files and elements copied from Django nested Inlines

--- rest_admin/__init__.py
+++ rest_admin/__init__.py
@@ -1,12 +1,14 @@
 from django.utils.module_loading import autodiscover_modules
 
-from rest_admin.options import RestAdmin, StackedRestInline, TabularRestInline
+from rest_admin.options import RestAdmin, StackedRestInline, TabularRestInline,\
+    NestedStackedInline, NestedTabularInline
 from rest_admin.sites import RestAdminSite, site
 
 
 __all__ = [
     "RestAdminSite", "autodiscover",
-    "RestAdmin", "StackedRestInline", "TabularRestInline"
+    "RestAdmin", "StackedRestInline", "TabularRestInline", "NestedStackedInline",
+    "NestedTabularInline"
 ]
 
 
